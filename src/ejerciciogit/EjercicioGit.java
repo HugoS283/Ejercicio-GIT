9c2f4e1a53ef5b62861190fa68653bb653c5fa40
dam <dam@203P01>
2023-02-02 09:33:44 +0100

    Lineas en el metodo main

---
+++ src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author dam
+ */
+public class EjercicioGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hola");
+        System.out.println("1");
+    }
+    
+}
